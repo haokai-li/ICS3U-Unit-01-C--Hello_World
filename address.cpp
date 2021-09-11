472587e29d00f3be4afae566cba13fb35fa72e55
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-09-11 02:30:05 +0000

    revise 1

--- address.cpp
+++ address.cpp
@@ -1,8 +1,13 @@
+// Copyright (c) 2021 haokai All rights reserved
+//
+// Created by: Haokai
+// Created on: Sept 2021
+// This is the "Hello world!" program, with proper style
+
 #include <iostream>
 
-int main()
-{
-	std::cout << "Haokai" << std::endl;
-	std::cout << "Ottawa" << std::endl;
-	std::cout << "Canada" << std::endl;
+int main() {
+	std::cout << "Haokai" << std::endl; 
+	std::cout << "Ottawa" << std::endl; 
+	std::cout << "Canada" << std::endl; 
 }
